d183def43ee9d1b2f01f52dec046f97099d1c664
joseph skokan <joeskokan20@gmail.com>
2024-12-12 23:35:22 -0500

    more css changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -34,94 +34,24 @@ function Home() {
       {/* Hero Section */}
       <section className="hero">
         <h1>Greetings, Traveler!</h1>
-        <h4>Location: 🌊West Palm Beach, Florida🌴</h4>
-        <p>Software engineer skilled in React, JavaScript, Python, and more.</p>
-        <Link to="/portfolio" className="button-main">
-          See My Work
-        </Link>
       </section>
 
       {/* Portfolio Section */}
       <section id="portfolio" className="portfolio">
-        <h2>Featured Projects</h2>
-        <p>Check out some of my recent work.</p>
+        <h2>WHOAMI?</h2>
+        {/* <p>Check out some of my recent work.</p> */}
 
         <div className="projects-container">
           {/* Project 1 */}
-          <div
-            className="project"
-            onMouseOver={playShakeSound} // Play sound on mouse over
-            onMouseOut={stopShakeSound} // Stop sound on mouse out
-          >
-            <a
-              href="https://github.com/notAnotherAverageJoe/Property-Preservation-Plus"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="/images/PPP.png"
-                alt="Property Preservation Plus"
-                className="project-image"
-              />
-              <h3>Property Preservation Plus</h3>
-              <p>
-                Project Overview: Property Preservation Plus is a full-featured
-                property management application.
-              </p>
-              <h3>🔍</h3>
-            </a>
-          </div>
-
-          {/* Project 2 */}
-          <div
-            className="project"
-            onMouseOver={playShakeSound} // Play sound on mouse over
-            onMouseOut={stopShakeSound} // Stop sound on mouse out
-          >
-            <a
-              href="https://github.com/notAnotherAverageJoe/bit_buddy"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="/images/bb1.jpg"
-                alt="Bit Buddy Project"
-                className="project-image"
-              />
-              <h3>Bit Buddy</h3>
-              <p>
-                A cryptocurrency platform that allows you to buy, sell, and
-                stake cryptocurrencies in an educational environment.
-              </p>
-              <h3>🔍</h3>
-            </a>
-          </div>
-
-          {/* Project 3 */}
-          <div
-            className="project"
-            onMouseOver={playShakeSound} // Play sound on mouse over
-            onMouseOut={stopShakeSound} // Stop sound on mouse out
-          >
-            <a
-              href="https://github.com/notAnotherAverageJoe/GG---GiggleGate-A-joke-paradise"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="/images/SQLfinance.png"
-                alt="Sql finance"
-                className="project-image"
-              />
-              <h3>SQL Financial System</h3>
-              <p>
-                Welcome to the Financial Transactions System! This project is a
-                robust and efficient solution for managing financial
-                transactions, deposits, withdrawals, transfers, and interest
-                calculations.
-              </p>
-              <h3>🔍</h3>
-            </a>
+          <div>
+            <h3>Joseph Skokan</h3>
+            <h4>Location: 🌊West Palm Beach, Florida🌴</h4>
+            <p>
+              Software engineer skilled in React, JavaScript, Python, and more.
+            </p>
+            <Link to="/portfolio" className="button-main">
+              See My Work
+            </Link>
           </div>
         </div>
       </section>
